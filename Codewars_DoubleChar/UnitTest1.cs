8baa334651c55e976ed268485575d1855a4550b6
darrenzhang <darrenzhang@91app.com>
2017-10-08 11:38:49 +0800

    Failed First test case

--- Codewars_DoubleChar/UnitTest1.cs
+++ Codewars_DoubleChar/UnitTest1.cs
@@ -6,8 +6,20 @@ namespace Codewars_DoubleChar
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_StringEmpty_Should_Be_StringEmpty()
         {
+            var input = string.Empty;
+            var actual = Kata.DoubleChar(input);
+            var expected = string.Empty + string.Empty;
+            Assert.AreEqual(expected,actual);
+        }
+    }
+
+    public class Kata
+    {
+        public static string DoubleChar(string input)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
